2ac6fcb383c557d2649e75f283d1b5dae872b293
ssdharma <ssdharma@ncsu.edu>
2016-12-28 22:21:32 +0530

    adding user inputs

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
+import sys
 import json
 from github import GitHub
 
+#git_user='subodh-dharma'
 #from agithub import GitHub
 git_token = os.environ['GITTOKEN']
-
-#gitapi = GitHub(token=git_token)
-#gitapi.users.get();
 gitapi = GitHub(access_token=git_token)
+# REF: http://stackoverflow.com/questions/230751/how-to-flush-output-of-python-print
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+git_user = raw_input("Enter Git User Name: ")
 # GET /users/:user
-user_info = gitapi.users('subodh-dharma').get()
+user_info = gitapi.users(git_user).get()
 
-#GET /user/repos
-repos = gitapi.users('subodh-dharma').repos().get()
+#GET /users/:user/repos
+repos = gitapi.users(git_user).repos().get()
 #repos_print = json.loads(repos)
 #print json.dumps(repos, indent=4, sort_keys=True)
 #print repos[0]['name']
